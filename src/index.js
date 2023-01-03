386fbe8820792d38aeb1a16e43a3dab0726fe463
yusufstawan <yusufsetiyawan45@gmail.com>
2023-01-03 21:48:16 +0700

    create component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
+import ContactApp from "./ContactApp";
 
-const element = <h1>Hello, World!</h1>
 
 const root = createRoot(document.getElementById("root"));
-root.render(element);
+root.render(<ContactApp />);
