55f756381c959240b1ba418ca7f0a0f3d9c761bf
yusufstawan <yusufsetiyawan45@gmail.com>
2023-01-09 20:35:25 +0700

    test from another project

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 import ContactApp from "./components/ContactApp";
 
-// styling
 import './styles/style.css'
 
 const root = createRoot(document.getElementById("root"));
