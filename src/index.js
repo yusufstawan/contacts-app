312d0af90ff243383492c50f4cf0bee744b4057b
yusufstawan <yusufsetiyawan45@gmail.com>
2023-01-12 23:06:35 +0700

    add page use react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import ContactApp from "./components/ContactApp";
 
 import './styles/style.css'
 
 const root = createRoot(document.getElementById("root"));
-root.render(<ContactApp />);
+root.render(
+  <BrowserRouter>
+    <ContactApp />
+  </BrowserRouter>
+);
---
+++ src/pages/AddPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { addContact } from '../utils/data';
+import ContactInput from '../components/ContactInput';
+import { useNavigate } from 'react-router-dom';
+
+function AddPage() {
+  const navigate = useNavigate();
+
+  function onAddContactHandler(contact) {
+    addContact(contact)
+    navigate('/');
+  }
+
+  return (
+    <section>
+      <h2>Tambah kontak</h2>
+      <ContactInput addContact={onAddContactHandler} />
+    </section>
+  )
+}
+
+export default AddPage;
