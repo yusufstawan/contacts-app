8012d2fe24109a9f3d555af2c18b8856f3a2592a
yusufstawan <yusufsetiyawan45@gmail.com>
2023-01-13 22:02:19 +0700

    add url search & react icon

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
 import ContactList from '../components/ContactList';
 import SearchBar from "../components/SearchBar";
 import { deleteContact, getContacts } from '../utils/data';
+
+function HomePageWrapper() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const keyword = searchParams.get('keyword');
+
+  function changeSearchParams(keyword) {
+    setSearchParams({ keyword });
+  }
+
+  return <HomePage defaultKeyword={keyword} keywordChange={changeSearchParams} />
+}
 
 class HomePage extends React.Component {
   constructor(props) {
@@ -9,7 +22,7 @@ class HomePage extends React.Component {
 
     this.state = {
       contacts: getContacts(),
-      keyword: '',
+      keyword: props.defaultKeyword || '',
     }
 
     this.onDeleteHandler = this.onDeleteHandler.bind(this);
@@ -22,7 +35,7 @@ class HomePage extends React.Component {
     // update the contact state from data.js
     this.setState(() => {
       return {
-        contacts: getContacts()
+        contacts: getContacts(),
       }
     })
   }
@@ -33,6 +46,8 @@ class HomePage extends React.Component {
         keyword
       }
     })
+
+    this.props.keywordChange(keyword);
   }
 
   render() {
@@ -52,4 +67,4 @@ class HomePage extends React.Component {
   }
 }
 
-export default HomePage;
+export default HomePageWrapper;
